a5b458f00ab7081bfb9591c88e9e38f9df7daad5
wangze0618 <2867273828@qq.com>
2022-01-14 22:19:47 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
 // vue脚手架项目 - 默认的配置文件名
+const CompressionWebpackPlugin = require("compression-webpack-plugin")
+
 // webpack+node环境 - 导出配置对象
 module.exports = {
+    configureWebpack: {
+        plugins: [
+            new CompressionWebpackPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: new RegExp('\\.(js|css)$'),
+                threshold: 10240,
+                minRatio: 0.8
+            })
+        ]
+    },
     devServer: {
         port: 3000,
         open: true // 浏览器自动打开
